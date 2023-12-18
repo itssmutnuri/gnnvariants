933b0a275c1765c376dd0ec26aae888a435c9b8a
Majd Al Aawar <malaawar@usc.edu>
2023-12-17 22:30:06 -0800

    Add files via upload

---
+++ modulefuncs/path_values.py
@@ -0,0 +1,15 @@
+PATH_PROCESSED_THRES_1_9th = r"data/NEW_DATA_RetroS_1_9th.csv"
+PATH_PROCESSED_THRES_0_25 = r"data/NEW_DATA_RetroS_0.25.csv"
+PATH_PROCESSED_THRES_1_3 = r"data/NEW_DATA_RetroS.csv"
+PATH_PROCESSED_THRES_0_5 = r"data/NEW_DATA_RetroS_0.5.csv"
+PATH_PROCESSED_THRES_1 = r"data/NEW_DATA_RetroS_1.csv"
+
+PATH_GROWTH_RATES = r"data/growth_rates_clustered_new.csv"
+
+PATH_VAR_22MAY = r"data/global_vars_May22.csv"
+PATH_TRAVEL_CONTROLS = r"data/international_travel_covid.csv"
+
+PATH_COUNTRY_ADJ = 'data/country_adj_fullname.json'
+
+PATH_ROUTES = r"data/routes.dat"
+PATH_AIRPORTS = r"data/airports.dat"
